fed2dfc93401c620bb29463c65648cba93ec3de8
Roger <rw@world-traveller.org>
2011-10-27 15:30:28 -0300

    updated paper compiler

--- paper/compile.sh
+++ paper/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pdflatex NEAT_paper2.tex
-bibtex NEAT_paper2
-pdflatex NEAT_paper2.tex
-pdflatex NEAT_paper2.tex
+pdflatex NEAT_paper.tex
+bibtex NEAT_paper
+pdflatex NEAT_paper.tex
+pdflatex NEAT_paper.tex
