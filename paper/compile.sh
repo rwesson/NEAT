e7141b52666af21768f6c8459e82f5820573e32c
Roger <rw@world-traveller.org>
2011-12-01 14:18:22 -0300

    compiles both papers

--- paper/compile.sh
+++ paper/compile.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-pdflatex NEAT_paper.tex
-bibtex NEAT_paper
-pdflatex NEAT_paper.tex
-pdflatex NEAT_paper.tex
+pdflatex NEAT_paper_I.tex
+bibtex NEAT_paper_I
+pdflatex NEAT_paper_I.tex
+pdflatex NEAT_paper_I.tex
+
+pdflatex NEAT_paper_II.tex
+bibtex NEAT_paper_II
+pdflatex NEAT_paper_II.tex
+pdflatex NEAT_paper_II.tex
