06c3df1e9cf4a4681b710f53ae18d70223cf4b8f
Roger <rw@world-traveller.org>
2011-12-05 11:25:47 -0300

    Useful additional scripts; linefinder to assist in preparing input files, bin.sh to produce plots of output histograms

---
+++ utilities/bin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+prefix=$1
+
+for filename in $prefix*
+do 
+ step=`more $filename | sort -g | awk ' NR==500 { low=$1 } NR==9500 { high=$1 } END { print (high-low)/30 } '` 
+  export step 
+  if [ $step != 0 ]; then 
+    more $filename | sort -g | awk -v step=$step ' step>0 && NR>50 && NR<9950 { print step*int($1/step) } ' | uniq -c > binned_$filename
+    echo "set terminal png enhanced" > tmp.prg
+    echo "set output \"binned_"$filename".png" >> tmp.prg
+    echo "plot 'binned_"$filename"' using 2:1 w l" >> tmp.prg
+    gnuplot tmp.prg
+  fi
+done
+
+rm tmp.prg
