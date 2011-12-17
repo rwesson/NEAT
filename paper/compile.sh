60491012175df7f72f1b4fc4efc98800cb126ac9
Roger <rw@world-traveller.org>
2011-12-17 15:43:30 -0300

    Updates.  Paper title changed to better reflect paper content

--- paper/compile.sh
+++ paper/compile.sh
@@ -5,7 +5,7 @@ bibtex NEAT_paper_I
 pdflatex NEAT_paper_I.tex
 pdflatex NEAT_paper_I.tex
 
-pdflatex NEAT_paper_II.tex
-bibtex NEAT_paper_II
-pdflatex NEAT_paper_II.tex
-pdflatex NEAT_paper_II.tex
+#pdflatex NEAT_paper_II.tex
+#bibtex NEAT_paper_II
+#pdflatex NEAT_paper_II.tex
+#pdflatex NEAT_paper_II.tex
