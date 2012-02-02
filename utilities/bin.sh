a0e8df0bb2f81c91e974cf522fe021ce8d0f52c6
rwesson <rwesson@eso.org>
2012-02-02 12:19:47 -0400

    Removed PDF manual, all information is now on the website. Moved line list to utilities directory.

--- utilities/bin.sh
+++ utilities/bin.sh
@@ -8,11 +8,11 @@ do
   export step 
   if [ $step != 0 ]; then 
     more $filename | sort -g | awk -v step=$step ' step>0 && NR>50 && NR<9950 { print step*int($1/step) } ' | uniq -c > binned_$filename
-    echo "set terminal png enhanced" > tmp.prg
-    echo "set output \"binned_"$filename".png" >> tmp.prg
+    echo "set terminal postscript enhanced" > tmp.prg
+    echo "set output \"binned_"$filename".ps" >> tmp.prg
     echo "plot 'binned_"$filename"' using 2:1 w l" >> tmp.prg
     gnuplot tmp.prg
   fi
 done
-
+mogrify -density 150 -format jpg -flatten *.ps
 rm tmp.prg
