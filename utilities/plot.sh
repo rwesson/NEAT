b3a7c486684fcfc74ea3347e7cbb3381bbcebfd5
Roger Wesson <rwesson@eso.org>
2012-10-14 01:33:19 -0300

    removed commented out lines

--- utilities/plot.sh
+++ utilities/plot.sh
@@ -8,7 +8,4 @@ do
   echo "set output \""$filename".png\"" >> tmp.prg
   echo "plot '"$filename"' every :::1::1 w l, '"$filename"' every :::0::0 using 1:(200):2:3 w xerr" >> tmp.prg
   gnuplot tmp.prg
-done
-
-#mogrify -density 150 -format png -rotate 90 -flatten *.ps
-#rm tmp.prg# *.ps
+done 
