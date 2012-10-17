c48cbf080865e2ee71274d230d5a7438000b4800
Roger Wesson <rwesson@eso.org>
2012-10-17 01:59:57 -0300

    Bigger bins means higher counts

--- utilities/plot.sh
+++ utilities/plot.sh
@@ -6,7 +6,10 @@ for filename in $prefix*binned
 do
   echo "set terminal png enhanced" > tmp.prg
   echo "set output \""$filename".png\"" >> tmp.prg
-  echo "plot '"$filename"' every :::1::1 w l, '"$filename"' every :::0::0 using 1:(200):2:3 w xerr" >> tmp.prg
+#  echo "f(x)=a*exp((-(x-mu)**2)/(2*sigma**2))" >> tmp.prg
+#  head -n 1 $filename | awk ' { print "mu="$1" ; sigma="($3-$2)/2 } ' >> tmp.prg
+#  tail -n +3 $filename | sort -k 2 -g | tail -n 1 | awk ' { print "a="$2 } ' >> tmp.prg 
+  echo "plot '"$filename"' every :::1::1 w l, '"$filename"' every :::0::0 using 1:(800):2:3 w xerr" >> tmp.prg
   gnuplot tmp.prg
 done
 
